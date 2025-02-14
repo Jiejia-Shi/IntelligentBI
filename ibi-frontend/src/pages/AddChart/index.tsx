88bf9ae0d4ae8ce9ca8b0c6f0e03d567295b5e92
Jiejia-Shi <j279shi@uwaterloo.ca>
2025-02-13 22:16:27 -0500

    front-end: beautify style

--- ibi-frontend/src/pages/AddChart/index.tsx
+++ ibi-frontend/src/pages/AddChart/index.tsx
@@ -1,5 +1,5 @@
 import { UploadOutlined } from '@ant-design/icons';
-import { Button, Form, Input, message, Select, Space, Upload } from 'antd';
+import {Button, Card, Col, Form, Input, message, Row, Select, Space, Spin, Upload} from 'antd';
 import React, { useState } from 'react';
 import TextArea from 'antd/es/input/TextArea';
 import { genChartByAiUsingPost } from '@/services/ibi/chartController';
@@ -16,6 +16,8 @@ const AddChart: React.FC = () => {
       return;
     }
     setSubmitting(true);
+    setOption(undefined);
+    setChart(undefined);
     const params = {
       ...values,
       file: undefined,
@@ -43,63 +45,78 @@ const AddChart: React.FC = () => {
 
   return (
     <div className="add-chart">
-      <Form name="addChart" onFinish={onFinish} initialValues={{}}>
-        <Form.Item
-          name="goal"
-          label="Analysis Goal"
-          rules={[{ required: true, message: 'Please enter your analysis goal!' }]}
-        >
-          <TextArea placeholder="Please enter your analysis goal. For example, analyse the growth trend of users" />
-        </Form.Item>
-        <Form.Item
-          name="name"
-          label="Chart Name"
-          rules={[{ required: true, message: 'Please enter your chart name!' }]}
-        >
-          <Input placeholder="Please enter your chart name" />
-        </Form.Item>
-        <Form.Item name="chartType" label="Chart Type">
-          <Select
-            placeholder="Please select your chart type"
-            options={[
-              { value: 'Pie Chart', label: 'Pie Chart' },
-              { value: 'Bar Chart', label: 'Bar Chart' },
-              { value: 'Column Chart', label: 'Column Chart' },
-              { value: 'Line Chart', label: 'Line Chart' },
-              { value: 'Scatter Plot', label: 'Scatter Plot' },
-              { value: 'Histogram', label: 'Histogram' },
-              { value: 'Area Chart', label: 'Area Chart' },
-              { value: 'Radar Chart', label: 'Radar Chart' },
-            ]}
-          />
-        </Form.Item>
+      <Row gutter={24}>
+        <Col span={12}>
+          <Card title={"Intelligent Analysis"}>
+            <Form name="addChart" onFinish={onFinish} initialValues={{}}>
+              <Form.Item
+                name="goal"
+                label="Analysis Goal"
+                rules={[{ required: true, message: 'Please enter your analysis goal!' }]}
+              >
+                <TextArea placeholder="Please enter your analysis goal. For example, analyse the growth trend of users" />
+              </Form.Item>
+              <Form.Item
+                name="name"
+                label="Chart Name"
+                rules={[{ required: true, message: 'Please enter your chart name!' }]}
+              >
+                <Input placeholder="Please enter your chart name" />
+              </Form.Item>
+              <Form.Item name="chartType" label="Chart Type">
+                <Select
+                  placeholder="Please select your chart type"
+                  options={[
+                    { value: 'Pie Chart', label: 'Pie Chart' },
+                    { value: 'Bar Chart', label: 'Bar Chart' },
+                    { value: 'Column Chart', label: 'Column Chart' },
+                    { value: 'Line Chart', label: 'Line Chart' },
+                    { value: 'Scatter Plot', label: 'Scatter Plot' },
+                    { value: 'Histogram', label: 'Histogram' },
+                    { value: 'Area Chart', label: 'Area Chart' },
+                    { value: 'Radar Chart', label: 'Radar Chart' },
+                  ]}
+                />
+              </Form.Item>
 
-        <Form.Item
-          name="file"
-          label="Analysis Data"
-          rules={[{ required: true, message: 'Please upload your analysis data!' }]}
-        >
-          <Upload name="file">
-            <Button icon={<UploadOutlined />}>Click to upload data</Button>
-          </Upload>
-        </Form.Item>
+              <Form.Item
+                name="file"
+                label="Analysis Data"
+                rules={[{ required: true, message: 'Please upload your analysis data!' }]}
+              >
+                <Upload name="file" maxCount={1}>
+                  <Button icon={<UploadOutlined />}>Click to upload data</Button>
+                </Upload>
+              </Form.Item>
 
-        <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
-          <Space>
-            <Button type="primary" htmlType="submit" loading={submitting} disabled={submitting}>
-              Submit
-            </Button>
-            <Button htmlType="reset">reset</Button>
-          </Space>
-        </Form.Item>
-      </Form>
-      <div>Analysis Result: {chart?.genResult}</div>
-      <div>
-        Analysis Chart:
-        {
-          option && <ReactECharts option={option} />
-        }
-      </div>
+              <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
+                <Space>
+                  <Button type="primary" htmlType="submit" loading={submitting} disabled={submitting}>
+                    Submit
+                  </Button>
+                  <Button htmlType="reset">reset</Button>
+                </Space>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card title={"Analysis Result"}>
+            <div>
+              {chart?.genResult ?? <div>Please submit your analysis request on the left first</div>}
+              <Spin spinning={submitting}/>
+            </div>
+          </Card>
+          <Card title={"Generated Chart"}>
+            <div>
+              {
+                option ? <ReactECharts option={option} /> : <div>Please submit your analysis request on the left first</div>
+              }
+              <Spin spinning={submitting}/>
+            </div>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
